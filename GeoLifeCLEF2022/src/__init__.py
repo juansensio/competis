78a75828e55a4e610ecf6f7d18c602663ab7f72a
earthpulse <it@earthpulse.es>
2022-05-14 08:12:11 +0200

    update

--- GeoLifeCLEF2022/src/__init__.py
+++ GeoLifeCLEF2022/src/__init__.py
@@ -1,4 +1,4 @@
 from .ds import RGBDataset, RGBNirDataset, RGNirDataset, NirGBDataset, RGBNirBioDataset, RGBNirBioCountryDataset, AllDataset
 from .dm import RGBDataModule, RGBNirDataModule, RGNirDataModule, NirGBDataModule, RGBNirBioDataModule, RGBNirBioCountryDataModule, AllDataModule
-# from .utils import *
+from .utils import *
 from .module import RGBModule, RGBNirBioModule, RGBNirBioCountryModule, AllModule
